e99e8b0aced1110e0f8d54ba51dd9d1c42e110be
arthur.nascimento <arthur.nascimento@neomind.com.br>
2022-05-04 03:33:20 -0300

    Fade out para botão informativo e botão de fechar para alert de erro/validação

--- resources/js/home.js
+++ resources/js/home.js
@@ -4,7 +4,7 @@ function getEditItemTemplate(itemId, currentName, currentQuantity) {
         '                <input type="hidden" name="item_id" value="' + itemId + '">\n' +
         '                <div class="item rounded">\n' +
         '                    <div class="item-title">\n' +
-        '                        <h4>Nome: <input type="text" class="bg-transparent transparent-input" name="item_name" value="' + currentName + '"><br></h4>\n' +
+        '                        <h4>Nome: <input type="text" class="js-name-input bg-transparent transparent-input" name="item_name" value="' + currentName + '"><br></h4>\n' +
         '                    </div>\n' +
         '                    <div class="item-quantity">\n' +
         '                        <p>Quantidade: <input type="number" class="bg-transparent transparent-input" name="item_quantity" value="' + currentQuantity + '"></p>\n' +
@@ -23,11 +23,35 @@ function editItem(itemReference) {
 
     let editCard = getEditItemTemplate(itemId, currentName, currentQuantity);
     itemReference.replaceWith(editCard);
+    $(".js-name-input").focus();
 }
 
-$(document).ready(function () {
+function bindEditItem() {
     $(".js-edit-button").click(function (event) {
         event.preventDefault();
         editItem($(event.target.closest("div.item")));
     })
+}
+
+function bindInfoAlertFadeOut() {
+    $(".js-info-alert").fadeOut(3500, "swing");
+}
+
+function bindErrorAlertCloseButton() {
+    let closeAlertButtonReference = $(".js-close-alert-button");
+
+    closeAlertButtonReference.click(function (event) {
+        event.preventDefault();
+        $(".js-error-alert").remove();
+    })
+}
+
+function bindAlertActions() {
+    bindInfoAlertFadeOut();
+    bindErrorAlertCloseButton();
+}
+
+$(document).ready(function () {
+    bindEditItem();
+    bindAlertActions();
 })
--- src/alert.php
+++ src/alert.php
@@ -1,8 +1,8 @@
 <?php
     function successAlert($msg) {
-        return '<div class="alert alert-success" role="alert">' . $msg . '</div>';
+        return '<div class="alert alert-success js-info-alert" role="alert">' . $msg . '</div>';
     }
 
     function errorAlert($msg) {
-        return '<div class="alert alert-dark" role="alert">' . $msg . '</div>';
+        return '<div class="alert alert-dark js-error-alert" role="alert">' . $msg . '<button class="btn btn-danger js-close-alert-button"><i class="fa fa-times"></i></button></div>';
     }
